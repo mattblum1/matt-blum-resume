e230e792d209c9947d2af7b83649b12170748be3
Matt Blum <matt.blum@live.com>
2020-08-26 17:46:23 -0500

    - Update background image to accommodate a wider audience - Remove debugging lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'matt-blum-resume';
 
-  // scrollToId(id: string) {
-  //   const el = document.getElementById(id);
-  //   el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  // }
-
   scrollToId(selector: string, yOffset = -20) {
-    console.warn('selector', selector);
     const el = document.getElementById(selector);
-    console.warn('el', el);
     const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
     window.scrollTo({ top: y, behavior: 'smooth' });
