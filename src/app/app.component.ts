f912a59cbe1fa281b579e07fd0835a81f1f713bc
Matt Blum <matt.blum@live.com>
2020-08-26 17:15:25 -0500

    Add scroll down functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'matt-blum-resume';
+
+  scrollToId(id: string) {
+    const el = document.getElementById(id);
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
