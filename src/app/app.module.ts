a02d2df3c94bba5b28ca1885479364e6ac7ef619
Matt Blum <matt.blum@live.com>
2020-08-26 14:19:04 -0500

    - Add Material and create custom theme - Add Bootstrap Grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
