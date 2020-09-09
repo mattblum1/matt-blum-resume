d88006183c448b11cf475d106a2f40c9695198ee
Matt Blum <matt.blum@live.com>
2020-09-08 23:58:47 -0500

    Add logic to replace WEBP with JPG if the browser does not support WEBP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ import { ScrollSpyService } from 'ng-spy';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
+  JPG: string = 'jpg';
+  WEBP: string = 'webp';
   SECTION_Y_OFFSET: number = -16;
 
   isPastOneHundredViewHeight: boolean = false;
@@ -115,9 +117,9 @@ export class AppComponent implements AfterViewInit {
     private spyService: ScrollSpyService,
     private lightbox: Lightbox
   ) {
-    this.frontEndSkills = this.frontEndSkills.sort(
-      (a, b) => b.rating - a.rating
-    );
+    this.frontEndSkills = this.sortSkillsByRating(this.frontEndSkills);
+    this.librarySkills = this.sortSkillsByRating(this.librarySkills);
+    this.miscSkills = this.sortSkillsByRating(this.miscSkills);
   }
 
   ngAfterViewInit() {
@@ -136,23 +138,55 @@ export class AppComponent implements AfterViewInit {
       this.isPastOneHundredViewHeight =
         currentPageYOffset >= oneHundredViewHeight;
     });
-  }
 
-  openAlbum(album: IAlbum[], index: number): void {
-    this.lightbox.open(album, index);
+    // Update albums if browser does not support WEBP
+    const element = document.querySelector('html');
+    const doesSupportWebp: boolean = element.classList.contains('webp');
+
+    if (!doesSupportWebp) {
+      this.bullSearchWebAlbum = this.updateAlbumExtension(
+        this.bullSearchWebAlbum
+      );
+      this.acceleratedGeneticsAlbum = this.updateAlbumExtension(
+        this.acceleratedGeneticsAlbum
+      );
+      this.sireSearchAlbum = this.updateAlbumExtension(this.sireSearchAlbum);
+      this.shopSearchAlbum = this.updateAlbumExtension(this.shopSearchAlbum);
+      this.dairySireSearchAlbum = this.updateAlbumExtension(
+        this.dairySireSearchAlbum
+      );
+    }
   }
 
   closeAlbum(): void {
     this.lightbox.close();
   }
 
-  downloadPdfVersion(): void {}
+  openAlbum(album: IAlbum[], index: number): void {
+    this.lightbox.open(album, index);
+  }
+
+  replaceAll(str: string, find: string, replace: string) {
+    return str.replace(new RegExp(find, 'g'), replace);
+  }
 
   scrollToId(selector: string, yOffset: number = 0): void {
     const el = document.getElementById(selector);
     const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
     window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+
+  sortSkillsByRating(skills: Skill[]): Skill[] {
+    return skills.sort((a, b) => b.rating - a.rating);
+  }
+
+  updateAlbumExtension(album: IAlbum[]): IAlbum[] {
+    return album.map((a) => ({
+      src: this.replaceAll(a.src, this.WEBP, this.JPG),
+      caption: this.replaceAll(a.caption, this.WEBP, this.JPG),
+      thumb: this.replaceAll(a.thumb, this.WEBP, this.JPG),
+    }));
   }
 }
 
